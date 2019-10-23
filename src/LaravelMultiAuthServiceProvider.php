3618c07dc3e1dc2d8076e707558e544b2a1d6fdc
Al-Amin Firdows <alaminfirdows@gmail.com>
2019-10-23 16:07:13 +0600

    Initialize Package

---
+++ src/LaravelMultiAuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlAminFirdows\LaravelMultiAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMultiAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
