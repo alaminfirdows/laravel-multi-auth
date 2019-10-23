865c7e31e9867133c254e5d96bbeac60bf24023f
Al-Amin Firdows <alaminfirdows@gmail.com>
2019-10-23 18:30:09 +0600

    ServiceProvider updated

--- src/LaravelMultiAuthServiceProvider.php
+++ src/LaravelMultiAuthServiceProvider.php
@@ -1,21 +1,11 @@
 <?php
 
-namespace AlAminFirdows\LaravelMultiAuth;
+namespace AlAminFirdows\LaravelLaravelMultiAuth;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelMultiAuthServiceProvider extends ServiceProvider
+class LaravelLaravelMultiAuthServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap services.
      *
@@ -24,5 +14,80 @@ class LaravelMultiAuthServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerInstallCommand();
+        $this->registerAuthSettingsInstallCommand();
+        $this->registerAuthFilesInstallCommand();
+        $this->registerAuthModelInstallCommand();
+        $this->registerAuthViewsInstallCommand();
+        //$this->registerInstallMiddlewareCommand();
+    }
+
+    /**
+     * Register the laravel-multi-auth:install command.
+     */
+    private function registerInstallCommand()
+    {
+        $this->app->singleton('command.alaminfirdows.laravel-multi-auth.install', function ($app) {
+            return $app['AlAminFirdows\LaravelMultiAuth\Commands\LaravelMultiAuthInstallCommand'];
+        });
+
+        $this->commands('command.alaminfirdows.laravel-multi-auth.install');
+    }
+
+    /**
+     * Register the laravel-multi-auth:install command.
+     */
+    private function registerAuthSettingsInstallCommand()
+    {
+        $this->app->singleton('command.alaminfirdows.laravel-multi-auth.settings', function ($app) {
+            return $app['AlAminFirdows\LaravelMultiAuth\Commands\AuthSettingsInstallCommand'];
+        });
+
+        $this->commands('command.alaminfirdows.laravel-multi-auth.settings');
+    }
+
+    /**
+     * Register the laravel-multi-auth:install command.
+     */
+    private function registerAuthFilesInstallCommand()
+    {
+        $this->app->singleton('command.alaminfirdows.laravel-multi-auth.files', function ($app) {
+            return $app['AlAminFirdows\LaravelMultiAuth\Commands\AuthFilesInstallCommand'];
+        });
+
+        $this->commands('command.alaminfirdows.laravel-multi-auth.files');
+    }
+
+    /**
+     * Register the laravel-multi-auth:install command.
+     */
+    private function registerAuthModelInstallCommand()
+    {
+        $this->app->singleton('command.alaminfirdows.laravel-multi-auth.model', function ($app) {
+            return $app['AlAminFirdows\LaravelMultiAuth\Commands\AuthModelInstallCommand'];
+        });
+
+        $this->commands('command.alaminfirdows.laravel-multi-auth.model');
+    }
+
+    /**
+     * Register the laravel-multi-auth:install command.
+     */
+    private function registerAuthViewsInstallCommand()
+    {
+        $this->app->singleton('command.alaminfirdows.laravel-multi-auth.views', function ($app) {
+            return $app['AlAminFirdows\LaravelMultiAuth\Commands\AuthViewsInstallCommand'];
+        });
+
+        $this->commands('command.alaminfirdows.laravel-multi-auth.views');
     }
 }
